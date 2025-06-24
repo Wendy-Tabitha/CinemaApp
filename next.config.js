9f57b9ed1a262082d95eb78c40faa59116c1746a
Wendy-Tabitha <akinyiwendy46@gmail.com>
2025-06-24 17:57:57 +0300

    chore: initialize project with Next.js, Tailwind CSS, and TypeScript
    - Added package.json with project dependencies and scripts for development, build, and linting.
    - Created postcss.config.mjs to configure PostCSS with Tailwind CSS.
    - Added tailwind.config.js for Tailwind CSS configuration with custom theme and animations.
    - Initialized tsconfig.json for TypeScript configuration with strict settings and module resolution.

---
+++ next.config.js
@@ -0,0 +1,39 @@
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'www.omdbapi.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
